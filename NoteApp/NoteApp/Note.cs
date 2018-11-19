41b2f118713ccfc31711f2ed5aeba22ea1cb4dd6
notUniqueUsername <muternatallia@gmail.com>
2018-11-19 11:00:05 +0700

    Добавлены нужные классы

---
+++ NoteApp/NoteApp/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    public class Note : ICloneable
+    {
+        private string _name;
+        private NoteCategory _noteCategory;
+        private string _noteText;
+        private DateTime _timeChange;
+        private DateTime _timeCreate;
+
+        public object Clone()
+        {
+            return 1;
+        }
+    }
+}
