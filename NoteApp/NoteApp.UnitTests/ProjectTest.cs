023eba2410da5a8a82bb02bf9833c3394bb890ad
notUniqueUsername <muternatallia@gmail.com>
2019-01-13 20:30:33 +0700

    rename

--- NoteApp/NoteApp.UnitTests/ProjectTest.cs
+++ NoteApp/NoteApp.UnitTests/ProjectTest.cs
@@ -66,7 +66,7 @@ namespace NoteApp.UnitTests
 
         [Test]
         [TestCase(
-            TestName = "Проверка Sort при 0 элементов")]
+            TestName = "Проверка сортировки Project при 0 элементов")]
         public void TestProjectSort_0_elements()
         {
             var project = new Project();
